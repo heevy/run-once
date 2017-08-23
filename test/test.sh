2107b534c1e83fcbb0917f7dd1e347c4dc008abc
Jiří Dudek <jdk@Jiri-MacBook-Pro-2.local>
2017-08-23 15:11:53 +0200

    Add one-line version.

---
+++ test/test.sh
@@ -0,0 +1,1 @@
+../src/run-once /tmp/run-once-test 1s 'TEST_VAR="testVar";echo "Test var : $TEST_VAR"'
--- test/testCommand.sh
+++ test/testCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export LOCK_FILE=/tmp/run-once-test
+export LOCK_FILE=/tmp/run-once-command-test
 export MAX_LOCK_TIME="1s"
 export COMMAND='TEST_VAR="testVar";echo "Test var : $TEST_VAR"'
 source ../src/run-once-skeleton
